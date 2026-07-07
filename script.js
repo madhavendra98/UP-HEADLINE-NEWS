5ca4943eb77706fd40cd25403e5b5e564f9cb276
madhavendra98 <smartmadhavendra@gmail.com>
2026-07-07 15:01:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-import { ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
 import { db } from "./firebase-config.js";
 
 const newsContainer = document.querySelector(".news-container");
 
-function loadNews() {
-  const newsRef = ref(db, "news");
+const q = query(collection(db, "news"), orderBy("date", "desc"));
 
-  onValue(newsRef, (snapshot) => {
+onSnapshot(q, (snapshot) => {
+
     newsContainer.innerHTML = "";
 
-    snapshot.forEach((child) => {
-      let data = child.val();
+    snapshot.forEach((doc) => {
 
-      newsContainer.innerHTML += `
+        const data = doc.data();
+
+        newsContainer.innerHTML += `
+
         <div class="news-card">
-          <img src="${data.image}">
-          <h3>${data.title}</h3>
-          <p>${data.desc}</p>
-        </div>
-      `;
-    });
-  });
-}
 
-loadNews();
+            <img src="${data.image}" alt="News">
+
+            <h3>${data.title}</h3>
+
+            <p>${data.description}</p>
+
+            <small>${data.category}</small>
+
+        </div>
+
+        `;
+
+    });
+
+});
